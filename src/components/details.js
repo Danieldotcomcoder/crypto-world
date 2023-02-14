d45ff8558ee6079bb19f26cc402639afbdcbbf16
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-02-14 16:06:47 +0200

    Fix Table in details page

--- src/components/details.js
+++ src/components/details.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Sparklines, SparklinesLine, SparklinesBars, SparklinesReferenceLine, SparklinesSpots } from 'react-sparklines';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-// import TableHeader from '@material-ui/core/TableHeader';
-// import TableHeaderColumn from '@material-ui/core/TableHeaderColumn'
-import TableRow from '@material-ui/core/TableRow'
-// import TableRowColumn from '@material-ui/core/TableRowColumn'
-import parse from 'html-react-parser'
-  
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import parse from 'html-react-parser';
 
 
 const Details = () => {
@@ -16,6 +16,10 @@ const Details = () => {
 
   const location = useLocation();
   const { coin } = location.state;
+  function createData(rank, tier, symbol, marketcap,price,volume) {
+    return { rank, tier, symbol, marketcap,price,volume};
+  }
+  
 
   const fetchcoindata = async () => {
     const options = {
@@ -36,9 +40,16 @@ const Details = () => {
   };
   useEffect(() => {
     fetchcoindata();
+    
   }, []);
+ 
+  var rows = [];
   if (!data) return null;
-
+  else {
+    rows = [
+      createData(data.rank, data.tier, data.symbol, data.marketCap, data.price, data["24hVolume"]),
+    ];
+  }
   return (
     <div>
       <Link className="back" to="/">
@@ -57,10 +68,41 @@ const Details = () => {
         </li>
       </ul>
       <div className="table-div">
-        <Table>
-          {/* <TableHeader> */}
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Rank</TableCell>
+            <TableCell align="right">Tier</TableCell>
+            <TableCell align="right">Symbol</TableCell>
+            <TableCell align="right">Market Cap</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">24h Volume</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.rank}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.rank}
+              </TableCell>
+              <TableCell align="right">{row.tier}</TableCell>
+              <TableCell align="right">{row.symbol}</TableCell>
+              <TableCell align="right">{row.marketcap}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+              <TableCell align="right">{row.volume}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+        {/* <Table>
+          <TableHeader>
             <TableRow>
-              {/* <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>
+              <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>
                 Rank
               </TableHeaderColumn>
               <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>
@@ -77,12 +119,12 @@ const Details = () => {
               </TableHeaderColumn>
               <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>
                 24h Volume
-              </TableHeaderColumn> */}
+              </TableHeaderColumn>
             </TableRow>
-          {/* </TableHeader> */}
+          </TableHeader>
           <TableBody>
             <TableRow>
-              {/* <TableRowColumn>{data.rank}</TableRowColumn> 
+              <TableRowColumn>{data.rank}</TableRowColumn> 
               <TableRowColumn>{data.tier}</TableRowColumn>
               <TableRowColumn>{data.symbol}</TableRowColumn>
               <TableRowColumn>
@@ -95,10 +137,10 @@ const Details = () => {
                 {' '}
                 $
               </TableRowColumn>
-              <TableRowColumn>{Number(data["24hVolume"])}</TableRowColumn> */}
+              <TableRowColumn>{Number(data["24hVolume"])}</TableRowColumn>
             </TableRow>
           </TableBody>
-        </Table>
+        </Table> */}
         <div className="sparkline-details">
           <Sparklines className="spk" style={{ fill: 'none' }} data={data.sparkline} limit={30} width={1000} height={120}>
             <SparklinesLine color="black" />
--- src/components/stats.js
+++ src/components/stats.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-// import TableHeader from '@material-ui/core/TableHeader';
-// import TableHeaderColumn from '@material-ui/core/TableHeaderColumn'
-import TableRow from '@material-ui/core/TableRow'
-// import TableRowColumn from '@material-ui/core/TableRowColumn'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Stats = () => {
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+  
+  
   const [stats, setStats] = useState(null);
 
   const statistics = {
@@ -29,26 +43,55 @@ const Stats = () => {
   return (
     <div>
       <div className="stats-table">
-        <Table>
-          {/* <TableHeader> */}
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+        {/* <Table>
+          <TableHeader>
             <TableRow>
-              {/* <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total Coins</TableHeaderColumn>
+              <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total Coins</TableHeaderColumn>
               <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total Markets</TableHeaderColumn>
               <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total Exchanges</TableHeaderColumn>
               <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total MarketCap</TableHeaderColumn>
-              <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total 24hVolume</TableHeaderColumn> */}
+              <TableHeaderColumn style={{ color: 'black', fontSize: '2vh' }}>Total 24hVolume</TableHeaderColumn>
             </TableRow>
-          {/* </TableHeader> */}
+          </TableHeader>
           <TableBody>
             <TableRow>
-              {/* <TableRowColumn>{stats.totalCoins}</TableRowColumn>
+              <TableRowColumn>{stats.totalCoins}</TableRowColumn>
               <TableRowColumn>{stats.totalMarkets}</TableRowColumn>
               <TableRowColumn>{stats.totalExchanges}</TableRowColumn>
               <TableRowColumn>{stats.totalMarketCap}</TableRowColumn>
-              <TableRowColumn>{stats.total24hVolume}</TableRowColumn> */}
+              <TableRowColumn>{stats.total24hVolume}</TableRowColumn>
             </TableRow>
           </TableBody>
-        </Table>
+        </Table> */}
       </div>
     </div>
   );
